5569b3f343f094f519cadbbf5a5d6f2831ccf055
Tuomo Hartikainen <hartitu@gmail.com>
2011-03-24 10:46:26 +0200

    Fixed h_prime_table -function, removed similar functions using old structures.
    	modified:   hprime.c
    	modified:   hprime.h

--- src/hprime.c
+++ src/hprime.c
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 
 
+/* Calculates and returns the greatest common divisor as integer */
 int h_gcd(int a, int b) {
     if( b > a ){
 	int tmp = a;
@@ -13,7 +14,7 @@ int h_gcd(int a, int b) {
     return h_calc_gcd(&a, &b);
 }
 
-
+/* A helper function for calculating the gcd. Does the actual dirty work */
 static int h_calc_gcd(int *a, int *b) {
     if( *b == 0 ) {
    	return *a;
@@ -24,33 +25,13 @@ static int h_calc_gcd(int *a, int *b) {
     return h_calc_gcd(b, a);
 }
 
-struct h_pf *h_distinct_prime_factors(int num, struct h_pf *pf, struct h_pl *pl) {
-    if(num < 2) {
-	h_pf_add_factor(pf, num);
-    } else {
-	int pos = 0;
-	/* untill num is a prime */
-	while( ( pos = h_bin_search(pl->list, pl->size, num)) < 0) {
-	    if( num % pl->list[pos] == 0 ) {
-		h_pf_add_unique(pf, pl->list[pos]);
-		num /= pl->list[pos];
-	    } else {
-		pos++;
-	    }
-	}
 
-	/* add num to factor list */
-	h_pf_add_unique(pf, num);
-     
-    }
-    
-    return pf;
-}
-
-
+/* Brute-forces the given number.
+ * Returns 1 if prime, -1 if not
+ */
 int h_is_prime_brute(int a) {
     if(a < 2 || a%2 == 0)
-	return 0;
+	return -1;
 
     if(a == 2)
 	return 1;
@@ -59,7 +40,7 @@ int h_is_prime_brute(int a) {
     
     while( running <= a/2 ) {
 	if( a % running == 0 ) {
-	    return 0;
+	    return -1;
 	}
 	running += 2;
     }
@@ -68,10 +49,12 @@ int h_is_prime_brute(int a) {
 }
 
 
+/* Returns n first prime numbers as a int*
+ * int *primes _must_ be initialized beforehand */
 int* h_prime_table(int n, int *primes) {
 
     int *sieve;
-    int i, j;
+    int i, j, p;
  
     sieve = malloc(20*n*sizeof(int));
 
@@ -80,60 +63,18 @@ int* h_prime_table(int n, int *primes) {
     }
     
     primes[0] = 2;
-    
-    for(i=0; i<20*n; i++) {
-
-	if(sieve[i] == 1) {
-	    for(j=i+3+i*2; j<20*n; j+=3+i*2) {
-		sieve[j] = 0;
-	    }
+    primes[1] = 3;
+    for(i=2; i<n; i++) {
+	p = (primes[i-1]-1)/2;
+	for(j=p; j<20*n; j+=primes[i-1]) {
+	    sieve[j] = 0;
 	}
-    }
-    
-    for(i=0, j=1; j<n && i<20*n; i++) {
-	if(sieve[i] == 1) {
-	    primes[j] = 3+2*j;
-	    j++;
-	}
+	while( !sieve[++p] );
+	primes[i] = 2*p+1;
     }
 
     free(sieve);
 
     return primes;
-}
-
-struct h_pl *h_prime_table_s(int n, struct h_pl *pl) {
-    int *sieve;
-    int i, j;
- 
-    sieve = malloc(20*n*sizeof(int));
-
-    for(i=0; i<20*n; i++) {
-	sieve[i] = 1;
-    }
-    
-    primes[0] = 2;
-    
-    for(i=0; i<20*n; i++) {
-
-	if(sieve[i] == 1) {
-	    for(j=i+3+i*2; j<20*n; j+=3+i*2) {
-		sieve[j] = 0;
-	    }
-	}
-    }
-
-    h_pl_add_prime(pl, 2);
-    
-    for(i=0, j=1; j<n && i<20*n; i++) {
-	if(sieve[i] == 1) {
-	    h_pl_add_prime(3+2*j);
-	    j++;
-	}
-    }
-
-    free(sieve);
-    
-    return pl;
 }
 
--- src/hprime.h
+++ src/hprime.h
@@ -1,5 +1,3 @@
-#include "h_pf.h"
-#include "h_pl.h"
 
 /* Front-end function to call for calculating gcd */
 int h_gcd(int a, int b);
@@ -11,15 +9,6 @@ int h_gcd(int a, int b);
 static int h_calc_gcd(int *a, int *b);
 
 
-/* Calculates the distinct prime factors of a number
- * num is the number to factorize
- * *pf is the prime factor struct
- * *pl is the prime struct
- * returns prime factor struct pointer
- */
-struct h_pf *h_distinct_prime_factors(int num, struct h_pf *pf, struct h_pl *pl);
-
-
 /* Returns 1 if a is prime, 0 if it's not */
 int h_is_prime_brute(int a);
 
@@ -29,8 +18,4 @@ int h_is_prime_brute(int a);
  * primes is the preallocated table of integers
  */
 int* h_prime_table(int n, int *primes);
-
-
-/* Fills the struct with n first primes */
-struct h_pl *h_prime_table_s(int n, struct h_pl *pl);
 
