68e965eac20c347e52dc539b33935b441206d1ef
Tuomo Hartikainen <hartitu@gmail.com>
2011-04-22 14:46:35 +0300

    Add qsort for char and integer arrays
    	modified:   src/halgorithm.c
    	modified:   src/halgorithm.h

--- src/halgorithm.h
+++ src/halgorithm.h
@@ -21,6 +21,8 @@ int h_bin_search(int* arr, int len, int n);
 
 int* permutation(int *data, const unsigned int L, const unsigned int N);
 
+void h_qsort_int(int *arr, int n);
+void h_qsort_char (char *arr, int n);
 
 #endif /* HALGORITHM_H */
 
