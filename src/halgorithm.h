50ed64a63576d1512d8eb47504c65fa062826267
Tuomo Hartikainen <hartitu@gmail.com>
2012-09-14 13:28:56 +0300

    Remove qsort functions
    stdlib.h already contains implementation of a generic qsort, redundant
    to have an own implementation.

--- src/halgorithm.h
+++ src/halgorithm.h
@@ -7,8 +7,6 @@
 
 int* permutation(int *data, const unsigned int L, const unsigned int N);
 
-void h_qsort_int(int *arr, int n);
-void h_qsort_char (char *arr, int n);
 
 #endif /* HALGORITHM_H */
 
