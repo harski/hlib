15c05878c2ee3de136c2d497067f4943bd42c491
Tuomo Hartikainen <hartitu@gmail.com>
2012-09-30 22:34:42 +0300

    Add Doxygen documentation to the project
    Add a doxygen config file ./Doxyfile, and add doxygen comments to the
    src/hutil.h.

--- src/hutil.h
+++ src/hutil.h
@@ -1,6 +1,25 @@
 /* Copyright 2011-2012 Tuomo Hartikainen <hartitu@gmail.com>.
  * Licensed under the 2-clause BSD license. */
 
+/**
+ * \file hutil.h
+ * \author Tuomo Hartikainen
+ * \date 120930
+ * \version 0.1
+ * \brief Describes the functions in this library.
+ *
+ * \mainpage
+ * \section intro Introduction
+ * This is a miscallaneous utility library aiming to solve various
+ * problems I've encoutered in various projects.
+ *
+ * \section inst Installation
+ * \code
+ * make
+ * make install
+ * \endcode
+ * See the Makefile for options.
+ */
 
 #ifndef HUTIL_H
 #define HUTIL_H
@@ -15,52 +34,91 @@
 #define H_PRIMES_IN_INT64 9223372036854775808
 
 
-/* Front-end function to call for calculating gcd */
+/**
+ * \brief Calculate gcd of two numbers.
+ * \param a The first number to calculate the gcd from.
+ * \param b The second number to calculate the gcd from.
+ * \return The gcd of a and b.
+ */
 unsigned int gcd (unsigned int a, unsigned int b);
 
 
-/* Returns 1 if a is prime, 0 if it's not */
+/**
+ * \brief Brute force check for primeness of an integer.
+ * \param a The number to be checked.
+ * \return 1 if a is prime, 0 if it is not.
+ * \sa is_prime_table
+ * \sa prime_table_get_index
+ */
 int is_prime_brute (const unsigned int a);
 
 
-/* Checks if a given number is a prime by checking the table.
- * a is the number to be searched,
- * table is the array of primes,
- * len is the length of the prime table.
- * Returns a non-zero or 0 if depending wether the number was found.
+/**
+ * \brief Checks if a number is a prime by checking the supplied table.
+ *
+ * Search for a in table with binary search.
+ *
+ * \param a The number which primeness is checked.
+ * \param table The array of primes in ascending order.
+ * \param len The length of the prime table.
+ * \return 0 if a was not found, 1 if it was.
  */
-int is_prime_table(int a, int *table, int len);
+int is_prime_table (int a, int *table, int len);
 
 
-/* Raise unsigned int to a unsigned power. If uint cannot hold the result
- * 0 is returned instead of the real result */
-unsigned int pow_uint (const unsigned int base,  const unsigned int exp);
-
-
-/* Returns the index of the prime or -1 if it isn't in the table. */
-int prime_table_get_index(int a, int *table, int len);
-
-/* Returns the number of primes under n
- * *n is the upper limit
+/**
+ * \brief Raise unsigned int to a unsigned power.
+ * \param base The base number.
+ * \param exp The exponent.
+ * \return The result if it fits to an unsigned int, or 0 if it doesn't.
  */
-int primes_under(int *n);
+unsigned int pow_uint (const unsigned int base, const unsigned int exp);
 
 
-/* Returns a table of n first primes
- * n is the number of primes to get
- * primes is the preallocated table of integers
+/**
+ * \brief Get the index of the given prime number.
+ * \param a The prime number to be searched.
+ * \param table The array that is searched.
+ * \param len The length of the prime array.
+ * \return The index of a if it is found, otherwise -1.
  */
-int* prime_table(int n, int *primes);
+int prime_table_get_index (int a, int *table, int len);
 
 
-/* Fills the pre-allocated sieve with 0's and 1's, 1's marking the position of
- * the primes. */
+/**
+ * \brief Gives an upper limit of how much primes there are below n.
+ * \param n The upper limit.
+ * \return The upper limit of number of primes below n.
+ */
+int primes_under (int *n);
+
+
+/**
+ * \brief Get an array of n first primes.
+ * \param n The number of primes to get.
+ * \param primes At least n elements long pre-allocated array of integers.
+ * \return The filled primes array.
+ */
+int *prime_table (int n, int *primes);
+
+
+/**
+ * \brief Get size-long sieve of Erathosthenes (a prime sieve).
+ *
+ * \param sieve Size-long, preallocated char array.
+ * \param size The size of the sieve.
+ * \return Total number of primes in the sieve.
+ */
 int prime_sieve (char *sieve, size_t size);
 
 
-/* Fills the pre-allocated prime table with primes, based on the pre calculated
- * sieve.
- * Returns the number of primes found */
+/**
+ * \brief Get prime numbers from a sieve.
+ * \param sieve Sieve containing the primes.
+ * \param ssize Size (length) of the sieve.
+ * \param primes Pre-allocated, array big enough to hold the primes from the sieve.
+ * \return Number of primes in primes (and in sieve).
+ */
 int get_primes_from_sieve (char *sieve, size_t ssize, int *primes);
 
 
@@ -70,7 +128,16 @@ int get_primes_from_sieve (char *sieve, size_t ssize, int *primes);
 int* permutation(int *data, const unsigned int L, const unsigned int N);
 
 
-/* This function may only be used if x<sieve_len, or x is a prime */
+/**
+ * \brief Calculate phi(x) with a help of precalculated values.
+ * \param phis phis_len long array for holding calculated phi values.
+ * \param phis_len Length of phis.
+ * \param sieve sieve_len long prime sieve.
+ * \param sieve_len Length of sieve.
+ * \param x The number which phi value is computed.
+ * \return Phi value of x.
+ * \sa prime_sieve
+ */
 unsigned int phi_recursive (unsigned int *phis, size_t phis_len,
                             const char *sieve, size_t sieve_len,
                             const unsigned int x);
